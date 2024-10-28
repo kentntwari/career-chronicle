4f983b29ca80ef9a1c6681e3f1766f0f82db1a11
Kent Ntwari <designerjehovah@gmail.com>
2024-10-28 06:22:49 -0400

    refactor:abstract global nav

--- app.vue
+++ app.vue
@@ -2,23 +2,20 @@
   useHead({
     title: "Career Journey",
     htmlAttrs: {
+      lang: "en",
       class: "bg-site",
     },
     bodyAttrs: {
-      class: "font-body text-base",
+      class: "font-body text-base text-body",
     },
   });
 </script>
 
 <template>
   <div class="min-h-screen min-h-[100dvh] flex flex-col">
-    <nav class="bg-top-nav h-16 px-3">
-      <div class="w-full h-full container flex items-center justify-between">
-        <div class="font-bold text-md text-logo">CareerChronicle</div>
-      </div>
-    </nav>
-    <main class="flex-1 container">
+    <app-navigation></app-navigation>
+    <div class="flex-1 container">
       <NuxtPage />
-    </main>
+    </div>
   </div>
 </template>
