65579ec1c54c141bc0197ff1addd70d54362ac00
Kent Ntwari <designerjehovah@gmail.com>
2024-10-28 06:17:06 -0400

    feat:init app centric ui

---
+++ components/app/Navigation.vue
@@ -0,0 +1,70 @@
+<script lang="ts" setup>
+  const kinde = useKindeClient();
+  const { data: userData } = useAsyncData("kinde-user", async () => {
+    if (!kinde) return null;
+    return await kinde.getUser();
+  });
+
+  const toggleState = ref(false);
+
+  const { $auth } = useNuxtApp();
+</script>
+
+<template>
+  <nav class="bg-top-nav h-16 px-3 border-b-neutral-grey-600">
+    <div class="w-full h-full container flex items-center justify-between">
+      <div
+        class="relative font-bold text-md text-logo before:content-['ALPHA'] before:absolute before:-top-2 before:-right-6 before:font-medium before:text-3xs before:text-[#1751D0]"
+        role="Logo"
+        aria-label="CareerChronicle"
+      >
+        CareerChronicle
+      </div>
+      <figure
+        class="bg-[#fff] w-10 h-10 flex items-center justify-center rounded-full overflow-clip"
+      >
+        <dropdown-menu-root v-model:open="toggleState">
+          <client-only>
+            <dropdown-menu-trigger>
+              <lucide-user
+                v-if="!userData"
+                :size="20"
+                class="block text-neutral-grey-900"
+              />
+
+              <div v-else>
+                <img
+                  :src="!userData.picture ? '' : userData.picture"
+                  alt="avatar"
+                  loading="lazy"
+                  class="w-10 h-10"
+                />
+              </div>
+            </dropdown-menu-trigger>
+          </client-only>
+
+          <dropdown-menu-portal>
+            <dropdown-menu-content
+              class="min-w-28 outline-none bg-[#fff] rounded-md px-2 py-3 space-y-4 will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade text-sm text-neutral-grey-1000"
+              :side-offset="16"
+              :align="'end'"
+              :align-offset="-10"
+            >
+              <dropdown-menu-item>
+                <nuxt-link v-show="$auth.loggedIn" to="/api/logout" external
+                  >Sign out</nuxt-link
+                >
+                <nuxt-link v-show="!$auth.loggedIn" to="/api/login" external
+                  >Sign in</nuxt-link
+                >
+              </dropdown-menu-item>
+              <DropdownMenuItem v-show="$auth.loggedIn" class="text-danger-700"
+                >Delete account</DropdownMenuItem
+              >
+            </dropdown-menu-content>
+          </dropdown-menu-portal>
+        </dropdown-menu-root>
+      </figure>
+    </div>
+  </nav>
+</template>
---
+++ components/app/form/Organization.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+  import type { Orgs } from "~/types";
+
+  import { z } from "zod";
+  import { newOrg } from "~/utils/zschemas";
+
+  const emit = defineEmits<{
+    cancel: [void];
+    formSubmitted: [void];
+  }>();
+
+  const { errors, handleSubmit, defineField, isSubmitting } = useForm({
+    validationSchema: toTypedSchema(newOrg),
+  });
+
+  const previousOrgs = ref<Orgs>([]);
+
+  const { data: currentOrgs } = useNuxtData<Orgs>("orgs");
+
+  const onSubmit = handleSubmit((values) => {
+    const newOrg = {
+      name: values.name.toLowerCase(),
+      slug: generateSlug(values.name.toLowerCase()),
+    };
+
+    emit("formSubmitted");
+
+    $fetch("/api/user/organization", {
+      method: "POST",
+      body: newOrg,
+      onRequest() {
+        if (currentOrgs.value !== null) {
+          previousOrgs.value = currentOrgs.value;
+          currentOrgs.value.push(newOrg);
+        } else {
+          currentOrgs.value = [];
+          currentOrgs.value.push(newOrg);
+        }
+      },
+      onRequestError() {
+        currentOrgs.value = previousOrgs.value; // Rollback the data if the request failed.
+      },
+      async onResponse() {
+        await refreshNuxtData("orgs"); // Invalidate orgs in the background if the request succeeded.
+      },
+    });
+  });
+
+  const [org, orgAttrs] = defineField("name");
+</script>
+
+<template>
+  <form @submit="onSubmit" class="space-y-8">
+    <ui-input
+      type="text"
+      v-model="org"
+      v-bind="orgAttrs"
+      class="border-neutral-grey-500 outline-none"
+    />
+    <small class="text-xs text-danger-700">{{ errors.name }}</small>
+    <div class="w-full flex justify-end items-center gap-2">
+      <ui-button
+        variant="neutral"
+        class="h-9"
+        :disabled="isSubmitting"
+        @click="emit('cancel')"
+        >Cancel</ui-button
+      >
+      <ui-button type="submit" class="h-9 min-w-20" :disabled="isSubmitting"
+        >Save</ui-button
+      >
+    </div>
+  </form>
+</template>
