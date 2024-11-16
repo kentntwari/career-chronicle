fae10e588a29e7f41d76969e2c5d32ed1a943b7b
Kent Ntwari <designerjehovah@gmail.com>
2024-11-16 05:58:42 -0500

    feat: path regex match

---
+++ composables/useMatchOrganizationPath.ts
@@ -0,0 +1,9 @@
+export function useMatchOrganizationPath() {
+  const route = useRoute();
+
+  const isOrganizationPath = computed(() =>
+    /\/organization\/[\w]+/.test(route.path)
+  );
+
+  return isOrganizationPath;
+}
