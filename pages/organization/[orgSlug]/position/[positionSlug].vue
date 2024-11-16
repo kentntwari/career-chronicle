131372975a479f337742004faaa0db99d56e745e
Kent Ntwari <designerjehovah@gmail.com>
2024-11-16 06:05:15 -0500

    feat: init position page

---
+++ pages/organization/[orgSlug]/position/[positionSlug].vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+  import type { SingleOrg } from "~/types";
+
+  definePageMeta({
+    layout: false,
+  });
+</script>
+
+<template>
+  <NuxtLayout name="parent-organization">
+    <template #default="{ organization }: { organization: SingleOrg }">
+      <div v-if="!organization.hasCreatedPositionBefore">
+        There appears to be no result for this position
+        <app-create-position-btn />
+      </div>
+      <NuxtLayout
+        name="parent-position"
+        :parent-organization="organization.slug"
+      >
+        WDEW
+      </NuxtLayout>
+    </template>
+  </NuxtLayout>
+</template>
