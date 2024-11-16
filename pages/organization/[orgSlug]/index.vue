418c35411b0e9c91f40426f3141587e4bd20bbce
Kent Ntwari <designerjehovah@gmail.com>
2024-11-16 06:06:02 -0500

    feat:init global layouts

--- pages/organization/[orgSlug]/index.vue
+++ pages/organization/[orgSlug]/index.vue
@@ -1,150 +1,74 @@
 <script lang="ts" setup>
-  import type { UseFetchOptions } from "#app";
   import type { FetchResponse } from "ofetch";
-  import { useDelayedLoading } from "~/composables/useDelayedLoading";
   import type { SingleOrg, OrgPos } from "~/types";
+
+  import { useCurrentRouteOrg } from "~/composables/useCurrentRouteOrg";
 
   definePageMeta({
     middleware: ["protected"],
+    layout: false,
   });
 
-  const route = useRoute();
-  const positionUrlregex = /^\/api\/organization\/[\w-]+\/positions$/;
-  const isPositionsUrlMatch = positionUrlregex.test(route.path);
+  const nuxtApp = useNuxtApp();
 
-  const currentOrg = route.params.orgSlug;
+  const currentOrg = useCurrentRouteOrg();
+  const computedParams = computed(() => `/${currentOrg.value}/positions`);
 
-  const APIURL_BASE = "/api/organization";
-  const params = ref(
-    typeof currentOrg === "string" ? currentOrg : currentOrg.join("/")
-  );
-  const computedParams = computed(() => `/${params.value}/positions`);
-
-  const {
-    isLoading: isFetchingAllResources,
-    startLoading: beginFetch,
-    stopLoading: cancelFetch,
-  } = useDelayedLoading(500);
-
-  const OPTIONS_POSITIONS: UseFetchOptions<OrgPos> = {
-    key: "positions-" + params.value,
-    baseURL: APIURL_BASE,
-    immediate: false,
-    deep: false,
-    watch: [params],
-    onRequestError: () => cancelFetch(),
-    onResponse: () => cancelFetch(),
-    onResponseError: () => cancelFetch(),
-  } as const;
-
-  const OPTIONS_ORGANIZATION: UseFetchOptions<SingleOrg> = {
-    key: "org-" + params.value,
-    baseURL: APIURL_BASE,
-    deep: false,
-    onRequestError: () => cancelFetch(),
-    onResponseError: () => cancelFetch(),
-  } as const;
-
-  const nuxtData_positions = useNuxtData<OrgPos>(OPTIONS_POSITIONS.key!);
+  const toRefKey = useOrgPositionsKey();
+  const nuxtData_positions = useNuxtData<OrgPos>(toRefKey.value);
 
   const {
     data: positions,
-    status: isFetchingPositions,
+    status,
     execute: triggerFetchPositions,
-  } = useLazyFetch<OrgPos>(computedParams, OPTIONS_POSITIONS);
-
-  const {
-    data: organization,
-    error: errorFetchingOrganization,
-    status: isFetchingOrganization,
-  } = await useLazyFetch<SingleOrg>(`/${params.value}`, {
-    ...OPTIONS_ORGANIZATION,
-    onRequest: () => beginFetch(),
-    onResponse: ({ response }: { response: FetchResponse<SingleOrg> }) => {
-      if (response._data?.hasCreatedPositionBefore) triggerFetchPositions();
-      else cancelFetch();
+  } = useLazyFetch<OrgPos>(computedParams, {
+    key: toRefKey.value,
+    baseURL: "/api/organization",
+    deep: false,
+    immediate: false,
+    watch: false,
+    onRequestError: () => nuxtApp.$abortFetchPositions(),
+    onResponseError: () => nuxtApp.$abortFetchPositions(),
+    onResponse: ({ response }: { response: FetchResponse<OrgPos> }) => {
+      nuxtApp.$abortFetchPositions();
+      if (!nuxtData_positions.data.value)
+        nuxtApp.payload.data[toRefKey.value] = response._data;
+    },
+    getCachedData: (key, nuxtApp) => {
+      if (!nuxtApp.payload.data[key]) return;
+      return nuxtApp.payload.data[key];
     },
   });
 
-  const { isLoading: isDebouncedFetchingPositions } = useDebouncedLoading(
-    isFetchingPositions,
-    { minLoadingTime: 300 }
-  );
+  const { isLoading } = useDebouncedLoading(status, { minLoadingTime: 300 });
 
   watchEffect(() => {
-    switch (true) {
-      case !nuxtData_positions.data.value &&
-        organization.value?.hasCreatedPositionBefore:
-        triggerFetchPositions();
-        break;
-
-      case isPositionsUrlMatch:
-        triggerFetchPositions();
-
-      default:
-        break;
-    }
+    if (nuxtApp.$isFetchPositions.value) triggerFetchPositions();
   });
 </script>
 
 <template>
   <main>
-    <div v-if="isFetchingAllResources" data-allow-mismatch>
-      <app-skeleton-organization />
-    </div>
-    <div v-else data-allow-mismatch>
-      <template v-if="errorFetchingOrganization">
-        <small>{{ errorFetchingOrganization }}</small>
-      </template>
-      <template v-else>
-        <app-data-organization-pageHeader
-          :org-title="organization?.name ?? ''"
-          @selected="
-            (s) => {
-              params = s;
-            }
-          "
-        />
-
-        <app-skeleton-Positions
-          v-show="isDebouncedFetchingPositions === 'pending'"
-        />
-
-        <section
-          class="px-3 container"
-          v-show="isDebouncedFetchingPositions !== 'pending'"
-        >
-          <div class="mt-4" v-if="organization?.hasCreatedPositionBefore">
-            <app-data-organization-banner
-              :positions="positions?.length || '??'"
-            />
+    <NuxtLayout name="parent-organization">
+      <template #default="{ organization }: { organization: SingleOrg | null }">
+        <section class="container">
+          <div class="w-full" v-show="isLoading === 'pending'">
+            <app-skeleton-positions />
           </div>
           <div
-            class="mt-[4.5rem] max-w-[366px] text-balance font-medium"
-            v-else
+            class="w-full mt-4 px-3 container"
+            v-show="isLoading !== 'pending'"
           >
-            <p>
-              It seems you haven’t registered any checkpoint in your journey at
-              this organization.
-            </p>
-            <br />
-            <p>
-              Everyone gotta start somewhere. Just type in where you’re at the
-              moment with this organization.
-            </p>
-            <ui-dialog>
-              <template #trigger="{ open: createPosition }">
-                <ui-button class="mt-12" @click="createPosition()"
-                  >GET STARTED</ui-button
-                >
-              </template>
-              <template #default>
-                <app-form-position />
-              </template>
-            </ui-dialog>
+            <app-data-positions
+              :data="positions"
+              :parent-organization="organization?.slug ?? ''"
+              :is-first-position="
+                organization?.hasCreatedPositionBefore ? false : true
+              "
+            />
           </div>
         </section>
       </template>
-    </div>
+    </NuxtLayout>
   </main>
 </template>
