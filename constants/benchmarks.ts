b5ed0bc6e3a65fd3bc06a9a6541bde1bc0c8289e
Kent Ntwari <designerjehovah@gmail.com>
2024-11-28 13:19:47 -0500

    feat:init global level constants

---
+++ constants/benchmarks.ts
@@ -0,0 +1,4 @@
+export const ACHIEVEMENTS = "ACHIEVEMENTS" as const;
+export const PROJECTS = "PROJECTS" as const;
+export const FAILURES = "FAILURES" as const;
+export const CHALLENGES = "CHALLENGES" as const;
