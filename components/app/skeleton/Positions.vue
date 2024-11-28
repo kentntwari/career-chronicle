945b6c6ac415ff109cbbeea18d8851bfabc94ff2
Kent Ntwari <designerjehovah@gmail.com>
2024-11-28 13:05:43 -0500

    reafctor: rename skeleton

--- components/app/skeleton/Positions.vue
+++ components/app/skeleton/Positions.vue
@@ -1,9 +1,13 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { nanoid } from "nanoid";
+  import { cn } from "~/lib/cn";
+</script>
 
 <template>
-  <div class="px-3 py-10 container space-y-2">
+  <div :class="cn('px-3 py-10 container space-y-2', $attrs.class ?? '')">
     <div
       v-for="_el in 3"
+      :key="nanoid"
       class="w-40 h-5 bg-neutral-grey-600 animate-pulse rounded-md"
     ></div>
   </div>
