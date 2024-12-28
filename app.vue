40e7c487e2014b601ff46fad9155f06839ba3c1a
Kent Ntwari <designerjehovah@gmail.com>
2024-12-28 13:31:27 -0500

    feat:init delete account flow

--- app.vue
+++ app.vue
@@ -25,9 +25,31 @@
 
 <template>
   <div class="min-h-screen min-h-[100dvh] flex flex-col">
-    <app-navigation></app-navigation>
+    <ui-dialog class="min-h-56 grid">
+      <template #trigger="{ open }">
+        <app-navigation @update:delete-account="open()" />
+      </template>
+      <template #default="{ close }">
+        <app-form-delete
+          :data="'delete-account'"
+          target="ACCOUNT"
+          @cancel="close()"
+        >
+          <div class="space-y-2">
+            <p class="font-semibold text-lg text-danger-900">Warning</p>
+            <span class="block text-neutral-grey-1000"
+              >This is an irreversible action that will result in the permanent
+              deletion of your account from our servers and the loss of your
+              data</span
+            >
+          </div>
+        </app-form-delete>
+      </template>
+    </ui-dialog>
     <div class="flex-1 flex flex-col">
-      <NuxtLayout> <NuxtPage /></NuxtLayout>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
     </div>
   </div>
 </template>
