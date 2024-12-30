00a6b5cb233a0dba717adeb9c3ba8ec7b5216c5b
Kent Ntwari <designerjehovah@gmail.com>
2024-12-30 18:50:14 -0500

    feat: extend use

--- utils/stringify.ts
+++ utils/stringify.ts
@@ -1,7 +1,11 @@
-import type { RouteParamsGeneric } from "vue-router";
+import type { RouteParamsGeneric, LocationQueryValue } from "vue-router";
 export function stringifyRoute(
-  r: RouteParamsGeneric[keyof RouteParamsGeneric]
+  r:
+    | RouteParamsGeneric[keyof RouteParamsGeneric]
+    | LocationQueryValue
+    | LocationQueryValue[]
 ) {
   if (typeof r === "string") return r;
+  else if (!r) return "";
   return r.join("/");
 }
