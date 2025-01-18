ed127a2faf12c98271928471112eef8cf42edcd7
Kent Ntwari <designerjehovah@gmail.com>
2025-01-18 10:44:49 -0500

    refactor: remove context menu

--- components/app/data/benchmarks/Snippet.vue
+++ components/app/data/benchmarks/Snippet.vue
@@ -14,11 +14,6 @@
     parentBenchmark: Benchmark;
   }>();
 
-  const emit = defineEmits<{
-    edit: [void];
-    delete: [void];
-  }>();
-
   const computedClass = computed<string>(() => {
     switch (true) {
       case props.parentBenchmark.toLocaleLowerCase() ===
@@ -33,81 +28,25 @@
         return "";
     }
   });
-
-  const computedSingularBenchmarkName = computed(() => {
-    if (props.parentBenchmark === benchmarks.ACHIEVEMENTS) return "achievement";
-    if (props.parentBenchmark === benchmarks.PROJECTS) return "project";
-    if (props.parentBenchmark === benchmarks.FAILURES) return "failure";
-    if (props.parentBenchmark === benchmarks.CHALLENGES) return "challenge";
-  });
 </script>
 
 <template>
-  <context-menu-root>
-    <context-menu-trigger as-child>
-      <NuxtLink
-        v-if="data.slug !== ''"
-        :to="{
-          name: CURRENT_BENCHMARK,
-          params: {
-            orgSlug: parentOrganization,
-            positionSlug: parentPosition,
-            benchmark: parentBenchmark.toLocaleLowerCase(),
-          },
-          query: {
-            v: props.data.slug,
-          },
-        }"
-        class="block px-3 py-2 min-h-20 text-md rounded-lg"
-        :class="[computedClass]"
-      >
-        {{ data.title }}
-      </NuxtLink>
-    </context-menu-trigger>
-    <context-menu-portal>
-      <context-menu-content
-        class="min-w-[150px] z-30 bg-[#fff] outline-none rounded-md p-[4px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade space-y-1"
-        :side-offset="20"
-      >
-        <context-menu-item
-          value="Open new tab"
-          class="group text-xs leading-none text-neutral-grey-1000 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-4 select-none outline-none data-[disabled]:text-neutral-grey-600 data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-grey-300 data-[highlighted]:text-neutral-grey-1300"
-          @select="
-            async () =>
-              await navigateTo(
-                {
-                  name: CURRENT_BENCHMARK,
-                  params: {
-                    orgSlug: parentOrganization,
-                    positionSlug: parentPosition,
-                    benchmark: parentBenchmark.toLocaleLowerCase(),
-                  },
-                  query: {
-                    v: props.data.slug,
-                  },
-                },
-                {
-                  open: { target: '_blank' },
-                }
-              )
-          "
-        >
-          Open in new tab
-        </context-menu-item>
-        <context-menu-item
-          value="Change name"
-          class="group text-xs leading-none text-neutral-grey-1000 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-4 select-none outline-none data-[disabled]:text-neutral-grey-600 data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-grey-300 data-[highlighted]:text-neutral-grey-1300"
-        >
-          Edit {{ computedSingularBenchmarkName }}
-        </context-menu-item>
-        <context-menu-item
-          value="Delete Organization"
-          class="group text-xs leading-none text-danger-700 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-4 select-none outline-none data-[disabled]:text-neutral-grey-600 data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-grey-300 data-[highlighted]:text-neutral-grey-1300"
-          @select="emit('delete')"
-        >
-          Delete {{ computedSingularBenchmarkName }}
-        </context-menu-item>
-      </context-menu-content>
-    </context-menu-portal>
-  </context-menu-root>
+  <NuxtLink
+    v-if="data.slug !== ''"
+    :to="{
+      name: CURRENT_BENCHMARK,
+      params: {
+        orgSlug: parentOrganization,
+        positionSlug: parentPosition,
+        benchmark: parentBenchmark.toLocaleLowerCase(),
+      },
+      query: {
+        v: props.data.slug,
+      },
+    }"
+    class="block px-3 py-2 min-h-20 text-md rounded-lg"
+    :class="[computedClass]"
+  >
+    {{ data.title }}
+  </NuxtLink>
 </template>
