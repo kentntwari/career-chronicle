8a9be07bc7b7865dd94184393399a0d26dfea204
Kent Ntwari <designerjehovah@gmail.com>
2025-01-18 10:35:58 -0500

    fix: use custom alphabet

--- utils/slug.ts
+++ utils/slug.ts
@@ -1,7 +1,9 @@
 import slugify from "slugify";
-import { nanoid } from "nanoid";
+import { customAlphabet } from "nanoid";
 
 export function generateSlug(title: string) {
+  const alphabet = "0123456789_abcdefghijklmnopqrstuvwxyz-";
+  const nanoid = customAlphabet(alphabet, 12);
   const slug =
     slugify(title, {
       lower: true,
