3174428a695a44fef4a1ce04bc5db3d9dc6d8925
Kent Ntwari <designerjehovah@gmail.com>
2025-01-18 10:38:42 -0500

    feat: add intents

---
+++ constants/intents.ts
@@ -0,0 +1,4 @@
+export const EDIT_NAME = "EDIT_NAME" as const;
+export const EDIT_TITLE = "EDIT_TITLE" as const;
+export const EDIT_DESCRIPTION = "EDIT_DESCRIPTION" as const;
+export const EDIT_TIMELINE = "EDIT_TIMELINE" as const;
--- constants/routeNames.ts
+++ constants/routeNames.ts
@@ -1,3 +1,4 @@
+export const ALL_ORGANIZATIONS = "organizations";
 export const CURRENT_ORGANIZATION = "organization-orgSlug";
 export const CURRENT_POSITION = "organization-orgSlug-position-positionSlug";
 export const CURRENT_BENCHMARK =
