8ba65872ca1254e3401bf5fcb688c4d0d2044ffc
Kent Ntwari <designerjehovah@gmail.com>
2025-01-31 14:34:15 -0500

    feat: init privacy policy+terms and consitions

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  definePageMeta({
+    layout: "home",
+  });
+
+  useHead({
+    title: "Privacy policy | Career Journey",
+    htmlAttrs: {
+      lang: "en",
+    },
+  });
+</script>
+
+<template>
+  <div>Privacy policy</div>
+</template>
---
+++ pages/terms-and-conditions.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  definePageMeta({
+    layout: "home",
+  });
+
+  useHead({
+    title: "Terms and conditions | Career Journey",
+    htmlAttrs: {
+      lang: "en",
+    },
+  });
+</script>
+
+<template>
+  <div>Terms and conditions</div>
+</template>
