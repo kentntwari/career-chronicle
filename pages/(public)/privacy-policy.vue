af62f86049abd9454323dcec6e62304e176950db
Kent Ntwari <designerjehovah@gmail.com>
2025-02-04 10:13:54 -0500

    chore:update terms

---
+++ pages/(public)/privacy-policy.vue
@@ -0,0 +1,114 @@
+<script lang="ts" setup>
+  definePageMeta({
+    layout: "home",
+  });
+
+  useHead({
+    title: "Privacy policy | Career Journey",
+    meta: [
+      {
+        name: "description",
+        content: "Privacy policy for utilizing Career Journey",
+      },
+    ],
+  });
+</script>
+
+<template>
+  <div class="container nested-container mt-10 md:mt-20 text-neutral-grey-1300">
+    <h1 class="font-bold text-2xl">Privacy Policy</h1>
+    <p class="mt-2">Last updated: 2025-02-04</p>
+
+    <div class="mt-10 space-y-10">
+      <p>
+        At Career Chronicle ("we," "us," or "our"), we are committed to
+        protecting your privacy. This Privacy Policy explains how we collect,
+        use, disclose, and safeguard your information when you visit our
+        Platform.
+      </p>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">1. Information We Collect</h2>
+        <p>We may collect the following types of information:</p>
+        <ul class="list-disc list-inside">
+          <li>
+            <span class="font-medium">Personal Information: </span>
+            When you create an account, we collect personal information such as
+            your name and email address.
+          </li>
+          <li>
+            <span class="font-medium">Usage Data: </span>
+            We may collect information about how you access and use our
+            Platform, including your IP address, browser type, and pages
+            visited.
+          </li>
+        </ul>
+      </section>
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">2. How We Use Your Information</h2>
+        <p>
+          We may use the information we collect for various purposes, including:
+        </p>
+        <ul class="list-disc list-inside">
+          <li>To provide and maintain our Platform.</li>
+          <li>To notify you about changes to our Platform.</li>
+          <li>
+            To allow you to participate in interactive features of our Platform
+            when you choose to do so.
+          </li>
+          <li>To provide customer support.</li>
+          <li>
+            To gather analysis or valuable information so that we can improve
+            our Platform.
+          </li>
+        </ul>
+      </section>
+      <section class="flex flex-col gap-y-2">
+        <h2 class="font-bold text-lg">3. Disclosure of Your Information</h2>
+        <p>
+          We do not sell, trade, or otherwise transfer your personal information
+          to outside parties without your consent, except as required by law or
+          to protect our rights.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">4. Data Security</h2>
+        <p>
+          We take reasonable measures to protect your personal information from
+          unauthorized access, use, or disclosure. However, no method of
+          transmission over the internet or method of electronic storage is 100%
+          secure.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">5. Your Rights</h2>
+        <p>You have the right to:</p>
+        <ul class="list-disc list-inside">
+          <li>Access the personal information we hold about you.</li>
+          <li>Request correction of any inaccurate personal information.</li>
+          <li>Request deletion of your personal information.</li>
+        </ul>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">6. Changes to This Privacy Policy</h2>
+        <p>
+          We may update this Privacy Policy from time to time. Any changes will
+          be posted on this page with an updated effective date. We encourage
+          you to review this policy periodically to stay informed about our
+          practices.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">7. Contact Information</h2>
+        <p>
+          If you have any questions or concerns about this Privacy Policy,
+          please contact us at workwithkent@mail.com.
+        </p>
+      </section>
+    </div>
+  </div>
+</template>
---
+++ pages/(public)/terms-and-conditions.vue
@@ -0,0 +1,190 @@
+<script lang="ts" setup>
+  definePageMeta({
+    layout: "home",
+  });
+
+  useHead({
+    title: "Terms and conditions | Career Journey",
+    meta: [
+      {
+        name: "description",
+        content: "Terms and conditions for users of Career Journey",
+      },
+    ],
+  });
+</script>
+
+<template>
+  <div class="container nested-container mt-10 md:mt-20 text-neutral-grey-1300">
+    <h1 class="font-bold text-2xl">Terms and conditions</h1>
+    <p class="mt-2">Last updated: 2025-02-04</p>
+
+    <div class="mt-10 space-y-10">
+      <p>
+        Welcome to Career Chronicle (the "Platform"). By accessing or using our
+        Platform, you agree to comply with and be bound by these Terms and
+        Conditions. If you do not agree with these terms, please do not use our
+        Platform.
+      </p>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">1. Introduction</h2>
+        <p>
+          Career Chronicle is a career tracking and productivity tool designed
+          to help individuals document their professional journey in a
+          structured and organized way. Our Platform provides a private,
+          centralized space for users to reflect on their career progression,
+          streamline future CV updates, and make informed decisions about their
+          professional growth.
+        </p>
+      </section>
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">2. User Accounts</h2>
+        <p>
+          To take advantage of the features offered by our Platform, users must
+          create an account. Users must be individuals who have the right to
+          work in their country of origin. By creating an account, you agree to
+          provide accurate and complete information.
+        </p>
+      </section>
+      <section class="flex flex-col gap-y-2">
+        <h2 class="font-bold text-lg">3. Use of cookies</h2>
+        <p>
+          We use session cookies to keep users logged in. No tracking cookies
+          are used across the site. By using our Platform, you consent to the
+          use of cookies as described in this section.
+        </p>
+
+        <article class="mt-4 space-y-4">
+          <div class="space-y-2">
+            <h3 class="font-bold">Cookie Management Policy</h3>
+            <p>Last updated: 2025-02-03</p>
+          </div>
+
+          <section class="space-y-2">
+            <h4 class="font-bold">1. What Are Cookies?</h4>
+            <p>
+              Cookies are small text files that are stored on your device
+              (computer, tablet, or mobile) when you visit a website. They help
+              the website recognize your device and store certain information
+              about your preferences or past actions.
+            </p>
+          </section>
+
+          <section class="space-y-2">
+            <h4 class="font-bold">2. Types of Cookies We Use</h4>
+            <p>We use the following types of cookies on our Platform:</p>
+            <ul class="list-disc list-inside">
+              <li>
+                <span class="font-medium">Session Cookies: </span>
+                These cookies are temporary and are deleted from your device
+                when you close your browser. They are used to keep you logged in
+                while you navigate our Platform.
+              </li>
+            </ul>
+          </section>
+
+          <section class="space-y-2">
+            <h4 class="font-bold">3. Managing Cookies</h4>
+            <p>
+              You have the right to decide whether to accept or reject cookies.
+              You can manage your cookie preferences through your browser
+              settings. Most web browsers allow you to:
+            </p>
+            <ul class="list-disc list-inside">
+              <li>View the cookies stored on your device.</li>
+              <li>Delete cookies.</li>
+              <li>Block cookies from being set.</li>
+              <li>Set preferences for specific websites.</li>
+            </ul>
+            <p>
+              Please note that if you choose to block cookies, some features of
+              our Platform may not function properly.
+            </p>
+          </section>
+
+          <section class="space-y-2">
+            <h4 class="font-bold">2. Types of Cookies We Use</h4>
+            <p>We use the following types of cookies on our Platform:</p>
+            <ul class="list-disc list-inside">
+              <li>
+                <span class="font-medium"> 4. Changes to This Policy </span>
+                We may update this Cookie Management Policy from time to time.
+                Any changes will be posted on this page with an updated
+                effective date. We encourage you to review this policy
+                periodically to stay informed about our use of cookies.
+              </li>
+            </ul>
+          </section>
+        </article>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">4. Social Logins</h2>
+        <p>
+          You may register and log in to our Platform using social login
+          options, including Google, LinkedIn, and GitHub. When you use these
+          services, we will collect your name and email address. By using social
+          logins, you consent to the collection and use of this information.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">5. User Conduct</h2>
+        <p>
+          Users are prohibited from engaging in spamming or any abusive behavior
+          on the Platform. If such behavior is noticed, we reserve the right to
+          ban the user, terminate their account, and delete all saved
+          information.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">6. Intellectual Property</h2>
+        <p>
+          The Platform is built with open-source technology, and the code is
+          available for free as open source. There is no specific content that
+          we seek to protect.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">7. Limitation of Liability</h2>
+        <p>
+          Career Chronicle provides the Platform on an "as is" basis. We do not
+          make any warranties or representations regarding the accuracy,
+          reliability, or availability of the Platform. To the fullest extent
+          permitted by law, we shall not be liable for any direct, indirect,
+          incidental, or consequential damages arising from your use of the
+          Platform.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">8. Governing Law</h2>
+        <p>
+          These Terms and Conditions shall be governed by and construed in
+          accordance with the laws of Canada.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">9. Changes to Terms</h2>
+        <p>
+          If we make changes to these Terms and Conditions in the future, users
+          will be immediately notified when they try to access their account
+          with a message letting them know that the Terms and Conditions have
+          changed.
+        </p>
+      </section>
+
+      <section class="space-y-2">
+        <h2 class="font-bold text-lg">10. Contact Information</h2>
+        <p>
+          If you have any questions or concerns about these Terms and
+          Conditions, please contact us at workwithkent@mail.com.
+        </p>
+      </section>
+    </div>
+  </div>
+</template>
